9b9a91f9678b7c517dfc9600df6ab8b78b09d2e5
kinseyvo <kinseyvo@csu.fullerton.edu>
2024-07-03 11:51:29 -0700

    profile cards

--- AOA/src/Components/People.jsx
+++ AOA/src/Components/People.jsx
@@ -5,7 +5,11 @@ export default function People() {
 
     return (
         <div className={style.container}>
-            People Page
+            <div className={style.cardContainer}>
+                <div className={style.card}>Justin Duong</div>
+                <div className={style.card}>Shawn Nguyen</div>
+                <div className={style.card}>Kinsey Vo</div>
+            </div>
         </div>
     );
 };
