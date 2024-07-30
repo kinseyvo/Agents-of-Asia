416dc9f0aa859d149cfdd34548a43a22b3db9cc5
kinseyvo <kinseyvo@csu.fullerton.edu>
2024-07-29 17:43:07 -0700

    setting up year slider

--- AOA/src/Components/LorePage.jsx
+++ AOA/src/Components/LorePage.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "../styles/lore.module.css";
+
+function DropDownList() {
+    const [selectedValue, setSelectedValue] = useState("Option 1");
+    const handleChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
+
+    return (
+        <select value={selectedValue} onChange={handleChange}>
+            <option value="Option 1">Option 1</option>
+            <option value="Option 2">Option 2</option>
+            <option value="Option 3">Option 3</option>
+        </select>
+    );
+}
 
 export default function LorePage() {
 
     return (
         <div className={style.container}>
-            Lore Page
+            <div>
+                Lore Page
+            </div>
+            {DropDownList}
         </div>
     );
 };
