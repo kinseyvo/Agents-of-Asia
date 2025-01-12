8eb7db6ce8e1cb4179d437ba1ec4e0396f127df0
kinseyvo <kinseyvo@csu.fullerton.edu>
2025-01-11 22:47:03 -0800

    minor mobile fix

--- AOA/src/Components/LorePage.jsx
+++ AOA/src/Components/LorePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Slider from "@mui/material/Slider"
+import Slider from "@mui/material/Slider";
 import style from "../styles/lore.module.css";
 import lore from "../data/lore.js";
 
@@ -18,51 +18,51 @@ export default function LorePage() {
         <div className={style.container}>
             <div className={style.title}>
                 Lore Page
-                <div className={style.sliderContainer}>
-                    <Slider
-                        value={year}
-                        onChange={handleYearChange}
-                        valueLabelDisplay="auto"
-                        min={2001}
-                        max={2025}
-                        step={1}
-                        marks
-                    />
-                </div>
             </div>
-
+            <div className={style.sliderContainer}>
+                <Slider
+                    value={year}
+                    onChange={handleYearChange}
+                    valueLabelDisplay="auto"
+                    min={2001}
+                    max={2025}
+                    step={1}
+                    marks
+                />
+            </div>
             <div className={style.selectedYear}>
                 Selected Year: {year}
             </div>
-
-            {hasSelectedYear ? (
-                selectedLore ? (
-                    selectedLore.events.length > 0 ? (
-                        <div className={style.information}>
-                            {selectedLore.events.map((event, index) => (
-                                <div key={index} className={style.event}>
-                                    <h2>{event.name}</h2>
-                                    <p>{event.description}</p>
-                                    <p>{event.date}</p>
-                                    <img src={`../assets/${event.image}`} alt={event.name} className={style.image} />
-                                </div>
-                            ))}
-                        </div>
+            <div className={style.scrollContainer}>
+                {hasSelectedYear ? (
+                    selectedLore ? (
+                        selectedLore.events.length > 0 ? (
+                            <div className={style.information}>
+                                {selectedLore.events.map((event, index) => (
+                                    <div key={index} className={style.event}>
+                                        <h2>{event.name}</h2>
+                                        <p>{event.description}</p>
+                                        <p>{event.date}</p>
+                                        <img src={`../assets/${event.image}`} alt={event.name} className={style.image} />
+                                    </div>
+                                ))}
+                            </div>
+                        ) : (
+                            <div className={style.noLoreMessage}>
+                                No lore for this year unfortunately.
+                            </div>
+                        )
                     ) : (
                         <div className={style.noLoreMessage}>
                             No lore for this year unfortunately.
                         </div>
                     )
                 ) : (
-                    <div className={style.noLoreMessage}>
-                        No lore for this year unfortunately.
+                    <div className={style.defaultMessage}>
+                        Select a Year for Magical Lore
                     </div>
-                )
-            ) : (
-                <div className={style.defaultMessage}>
-                    Select a Year for Magical Lore
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 };
